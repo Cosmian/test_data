0ec833c6a196d671458c39f12b567ed7cdc1ff9d
Emmanuel Coste <emmanuel.coste@cosmian.com>
2025-08-18 16:09:07 +0200

    feat: add PKCS12 format for existing leaf.crt

--- certificates/csr/generate.sh
+++ certificates/csr/generate.sh
@@ -1,4 +1,6 @@
-#To generate a certificate authority (CA) that will sign an intermediate certificate with OpenSSL in the command line, you can use the following steps:
+#!/bin/sh
+
+# To generate a certificate authority (CA) that will sign an intermediate certificate with OpenSSL in the command line, you can use the following steps:
 
 ## Clean-up
 rm ca.*
@@ -11,15 +13,12 @@ rm leaf.*
 # This will generate a 2048-bit RSA private key for the CA. The private key will be stored in the file `ca.key`.
 openssl genrsa -out ca.key 3072
 
-
 #-2. Generate a self-signed certificate for the CA.**
 # This will generate a self-signed certificate for the CA that is valid for 365 days. The certificate will be stored in the file `ca.crt`.
 openssl req -new -x509 -key ca.key -out ca.crt -days 3650 -subj "/C=FR/ST=IdF/L=Paris/O=Cosmian/OU=R&D/CN=Test CA"
 
-
 #-3. Create a PKCS#12 file for the CA.**
 openssl pkcs12 -export -out ca.p12 -inkey ca.key -in ca.crt -passout pass:secret
-
 
 ## Intermediate
 
@@ -30,16 +29,13 @@ openssl pkcs12 -export -out ca.p12 -inkey ca.key -in ca.crt -passout pass:secret
 # - Basic Constraints: cA field must be set true; pathLenConstraint field SHOULD be present and SHOULD be 0
 # - CRL Distribution Points: At least one publicly accessible HTTP uniformResourceIdentifier must be present.
 
-
 #-1. Generate a private key for the intermediate certificate.**
 # This will generate a 2048-bit RSA private key for the intermediate certificate. The private key will be stored in the file `intermediate.key`.
 openssl genrsa -out intermediate.key 3072
 
-
 #-2. Generate a certificate signing request (CSR) for the intermediate certificate.**
 # This will generate a CSR for the intermediate certificate. The CSR will be stored in the file `intermediate.csr`.
 openssl req -new -key intermediate.key -out intermediate.csr -subj "/C=FR/ST=IdF/L=Paris/O=Cosmian/OU=R&D/CN=Test Intermediate"
-
 
 #-3. Sign the CSR with the CA's private key.**
 # To set the Key Usage and Extended Key Usage extensions and the CRL Distribution Point extension in the intermediate certificate, you can use the following options:
@@ -56,12 +52,9 @@ openssl req -new -key intermediate.key -out intermediate.csr -subj "/C=FR/ST=IdF
 # This will sign the CSR with the CA's private key and generate an intermediate certificate. The intermediate certificate will be stored in the file `intermediate.crt`.
 openssl x509 -req -in intermediate.csr -CA ca.crt -CAkey ca.key -out intermediate.crt -days 1825 -extensions v3_ca -extfile extensions.ext
 
-
-
 #-4. Create a PKCS#12 file for the intermediate certificate.**
 # This will create a PKCS#12 file for the intermediate certificate. The PKCS#12 file will contain the intermediate certificate's private key and certificate.
-openssl pkcs12 -export -out intermediate.p12 -inkey intermediate.key -in intermediate.crt  -passout pass:secret
-
+openssl pkcs12 -export -out intermediate.p12 -inkey intermediate.key -in intermediate.crt -passout pass:secret
 
 ## Leaf certificate
 
@@ -76,3 +69,5 @@ openssl req -new -key leaf.key -out leaf.csr -subj "/C=FR/ST=IdF/L=Paris/O=Cosmi
 #-3. Sign the CSR with the Intermediate's private key.**
 openssl x509 -req -in leaf.csr -CA intermediate.crt -CAkey intermediate.key -out leaf.crt -days 1825 -extensions v3_ca -extfile extensions.ext
 
+# Generate PKCS12 file from the leaf certificate and private key
+openssl pkcs12 -export -out leaf.p12 -inkey leaf.key -in leaf.crt -passout pass:secret
